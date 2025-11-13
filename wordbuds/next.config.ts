d30fb34ddd979f969ebd43075803fbe8cea76bbe
BADR Mohammed <badur2111@gmail.com>
2025-11-13 14:00:44 +0000

    BADR (Project structure , Tiptap Editor and Tiptap extensions   ) from 28:44 until 1:02:28

--- wordbuds/next.config.ts
+++ wordbuds/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Disable trace file to avoid OneDrive permission issues
+  experimental: {
+    instrumentationHook: false,
+  },
+  // Disable telemetry
+  telemetry: false,
 };
 
 export default nextConfig;
--- wordbuds/src/app/page.tsx
+++ wordbuds/src/app/page.tsx
@@ -1,11 +1,12 @@
-import {Button} from "@/components/ui/button";
-
-const Home = () =>{
-  return(
-    <div className = "flex min-h-screen items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  );
+import Link from "next/link";
+const Home = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      Click <Link href="/documents/123">
+      <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
+</Link > to go to document id
+</div >
+);
 }
 
 export default Home;
