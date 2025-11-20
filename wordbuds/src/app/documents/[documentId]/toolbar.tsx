04d9864c1cf519889bfd0f4707840fbaebbc0825
Jp Varanasi <59619220+JpVaranasi@users.noreply.github.com>
2025-11-20 11:16:56 +0000

    Jp(Tiptap Extensions,Editor store and tools)
    npm install @tiptap/react@2.10.2 @tiptap/starter-kit --legacy-peer-deps
    npm install @tiptap/extension-task-list@2.10.2 @tiptap/extension-task-item@2.10.2 --legacy-peer-deps
    npm install @tiptap/extension-table@2.10.2 @tiptap/extension-table-row@2.10.2 @tiptap/extension-table-header@2.10.2 @tip
    tap/extension-table-cell@2.10.2 --legacy-peer-deps
    npm install @tiptap/extension-image@2.10.2 --legacy-peer-deps
    npm install tiptap-extension-resize-image@1.2.1 --legacy-peer-deps
    npm install zustand@5.0.1 --legacy-peer-deps
    npm install @tiptap/extension-underline@2.10.2 --legacy-peer-deps

---
+++ wordbuds/src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import { useEditorStore } from "@/store/use-editor-store";
+import { Bold, BoldIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+
+interface ToolbarButtonProps{
+    onClick?: () => void;
+    isActive?: boolean;
+    icon:LucideIcon;
+}
+
+
+const ToolbarButton = ({
+    onClick,
+    isActive,
+    icon:Icon,
+}:ToolbarButtonProps) =>{
+    return (
+        <button 
+        onClick={onClick}
+        className={cn(
+            "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+            isActive && "bg-neutral-200/80"
+        )        
+        }
+        >
+          <Icon className="size-4" />
+        </button>
+    )
+}
+
+
+export const Toolbar = () => {
+    const {editor} = useEditorStore();
+    const sections :{ label:string; icon:LucideIcon;onClick:() => void,isActive?: boolean;}[][]=[
+        [
+            {
+                label:"Undo",
+                icon: Undo2Icon,
+                onClick:()=> editor?.chain().focus().undo().run(),
+
+            },
+            {
+                label:"Redo",
+                icon: Redo2Icon,
+                onClick:()=> editor?.chain().focus().redo().run(),
+
+            },
+            {
+                label:"Print",
+                icon: PrinterIcon,
+                onClick:()=> window.print(),
+
+            },
+            {
+                label:"Spell Check",
+                icon: SpellCheckIcon,
+                onClick:()=>{
+                    const current = editor?.view.dom.getAttribute("spellcheck");
+                    editor?.view.dom.setAttribute("spellcheck",current === "false"?"true":"fasle")
+                }
+
+            },
+        ],
+        [
+            {
+                label : "Bold",
+                icon: BoldIcon,
+                isActive: editor?.isActive("bold"),
+                onClick:() => editor?.chain().focus().toggleBold().run(),
+            },
+            {
+                label : "Italic",
+                icon: ItalicIcon,
+                isActive: editor?.isActive("italic"),
+                onClick:() => editor?.chain().focus().toggleItalic().run(),
+            },
+            {
+                label : "Underline",
+                icon: UnderlineIcon,
+                isActive: editor?.isActive("underline"),
+                onClick:() => editor?.chain().focus().toggleUnderline().run(),
+            }
+        ],
+        [
+            {
+                label : "Comment",
+                icon: MessageSquarePlusIcon,
+                onClick:() => console.log("a"),
+                isActive: false,
+            },
+            {
+                label : "List Todo",
+                icon: ListTodoIcon,
+                onClick:() => editor?.chain().focus().toggleTaskList().run(),
+                isActive: editor?.isActive("taskList"),
+            },
+            {
+                label : "Remove Formatting",
+                icon: RemoveFormattingIcon,
+                onClick:() => editor?.chain().focus().unsetAllMarks().run(),
+            },
+        ]
+    ]
+    return(
+        <div className = "bg-[#F1F4F9] px-2.5py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+            {sections[0].map((item)=>(
+                <ToolbarButton key={item.label} {...item} />
+            ))}
+             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+            {/*TODO: font fam*/}
+             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+            {/*TODO: headin*/}
+             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+            {/*TODO: font size*/}
+            <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+            {sections[1].map((item)=>(
+                <ToolbarButton key={item.label} {...item} />
+            ))}
+                        {/*TODO: text color */}
+                        {/*TODO: highlight color */}
+
+             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+            {/*TODO: link*/}
+            {/*TODO: image*/}
+            {/*TODO: align */}
+            {/*TODO: line height */}
+            {/*TODO: List*/}
+             <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+            {sections[2].map((item)=>(
+                <ToolbarButton key={item.label} {...item} />
+            ))}
+        </div>
+    )
+}
