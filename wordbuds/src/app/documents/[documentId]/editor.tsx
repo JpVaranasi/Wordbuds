dcd5154bea2b325a08e8039d1b33c074834e07d3
KayalShakir234589 <234589@buas.nl>
2025-12-13 01:06:38 +0300

    Finish editor updates (ruler + tiptap)
    Important: npm installs require --legacy-peer-deps

--- wordbuds/src/app/documents/[documentId]/editor.tsx
+++ wordbuds/src/app/documents/[documentId]/editor.tsx
@@ -19,6 +19,7 @@ import Underline from '@tiptap/extension-underline'
 import {useEditorStore} from '@/store/use-editor-store';
 import {FontSizeExtension } from '@/extensions/font-size';
 import { LineHeightExtension } from '@/extensions/line-height';
+import { Ruler } from './ruler';
 
 export const Editor = () => { 
 
@@ -112,6 +113,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler/> 
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0"> 
         <EditorContent editor={editor} />
       </div> 
